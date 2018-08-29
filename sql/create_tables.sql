a6f902a0c98696f813e634e3ba32b3a923d5734b
Sergei <sergei.visotsky@gmail.com>
2018-08-29 10:05:09 +0300

    SQL added

---
+++ sql/create_tables.sql
@@ -0,0 +1,27 @@
+﻿create table customers(
+   customer_id bigserial not null primary key,
+   first_name varchar(50),
+   last_name varchar(50),
+   age integer
+)
+
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.customers
+  OWNER TO service;
+
+create table orders(
+   order_id bigserial not null primary key,
+   customer_id bigserial not null references customers(customer_id) on delete cascade,
+   trans_id bigserial not null,
+   good varchar(50),
+   good_weight double precision,
+   price double precision
+)
+
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.orders
+  OWNER TO service;
