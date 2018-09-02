3a09c894631f8946f1489d3f8fa2f7550350336b
Sergei <sergei.visotsky@gmail.com>
2018-09-02 21:18:05 +0300

    Fully migrated to Spring

---
+++ SoapServer/src/main/java/org/sergei/soap/welcome/WelcomeController.java
@@ -0,0 +1,16 @@
+package org.sergei.soap.welcome;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping
+public class WelcomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String welcome() {
+        return "SOAP";
+    }
+}
