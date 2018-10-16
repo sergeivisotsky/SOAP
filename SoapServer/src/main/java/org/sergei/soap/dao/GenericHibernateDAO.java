f51471e16a94d737a0eaeede14e8102a519b61c7
Sergei <sergei.visotsky@gmail.com>
2018-10-16 13:08:22 +0300

    Persistence unit renamed

--- SoapServer/src/main/java/org/sergei/soap/dao/GenericHibernateDAO.java
+++ SoapServer/src/main/java/org/sergei/soap/dao/GenericHibernateDAO.java
@@ -11,7 +11,7 @@ import java.util.List;
 @SuppressWarnings("unchecked")
 public abstract class GenericHibernateDAO<T extends Serializable> {
 
-    private static final String PERSISTENCE_UNIT = "CustomerData";
+    private static final String PERSISTENCE_UNIT = "org.sergei.soap.jpa";
 
     private Class<T> persistentClass;
 
