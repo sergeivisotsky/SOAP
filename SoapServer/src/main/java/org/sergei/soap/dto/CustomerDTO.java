54e6b43e445fba5af95a25cdc435353d53513d33
Sergei <sergei.visotsky@gmail.com>
2018-10-29 15:27:34 +0200

    Copyright added

--- SoapServer/src/main/java/org/sergei/soap/dto/CustomerDTO.java
+++ SoapServer/src/main/java/org/sergei/soap/dto/CustomerDTO.java
@@ -4,7 +4,10 @@
 
 package org.sergei.soap.dto;
 
-import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
